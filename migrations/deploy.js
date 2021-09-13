6aaaf9da95407d2ea54abfbee234e4eb5dae89b8
Dana <dana@step.finance>
2021-09-13 15:17:58 -0400

    migrate script for mint

--- migrations/deploy.js
+++ migrations/deploy.js
@@ -9,5 +9,25 @@ module.exports = async function (provider) {
   // Configure client to use the provider.
   anchor.setProvider(provider);
 
-  // Add your deploy script here.
+  let program = anchor.workspace.RewardPool;
+
+  let authMintPubKeyString = process.env.POOL_AUTHORITY_MINT_PUBKEY;
+  if (authMintPubKeyString == undefined) {
+    throw "POOL_AUTHORITY_MINT_PUBKEY env var reqd"
+  }
+  let authMintPubkey = new anchor.web3.PublicKey(authMintPubKeyString);
+
+  const [ _configPubkey, _nonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from("config")], program.programId);
+  configPubkey = _configPubkey;
+  await program.rpc.initializeProgram(
+      _nonce,
+      authMintPubkey,
+      {
+          accounts: {
+              config: configPubkey,
+              payer: provider.wallet.publicKey,
+              systemProgram: anchor.web3.SystemProgram.programId,
+          },
+      }
+  );
 }
