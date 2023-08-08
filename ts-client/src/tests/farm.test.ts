c6b2df8b1b5923ac069f719c864e2264d141639f
McSam <mcsam1216@gmail.com>
2023-08-08 13:07:04 +0800

    uncomment test case

--- ts-client/src/tests/farm.test.ts
+++ ts-client/src/tests/farm.test.ts
@@ -32,102 +32,102 @@ const mockWallet = new Wallet(
     : new Keypair()
 );
 
-// describe("Interact with devnet farm", () => {
-//   const provider = new AnchorProvider(DEVNET.connection, mockWallet, {
-//     commitment: "confirmed",
-//   });
+describe("Interact with devnet farm", () => {
+  const provider = new AnchorProvider(DEVNET.connection, mockWallet, {
+    commitment: "confirmed",
+  });
 
-//   let farm: PoolFarmImpl;
-//   let lpBalance: BN;
-//   let stakedBalance: BN;
-//   beforeAll(async () => {
-//     await airDropSol(DEVNET.connection, mockWallet.publicKey).catch(() => {});
+  let farm: PoolFarmImpl;
+  let lpBalance: BN;
+  let stakedBalance: BN;
+  beforeAll(async () => {
+    await airDropSol(DEVNET.connection, mockWallet.publicKey).catch(() => {});
 
-//     const USDT = DEVNET_COIN.find(
-//       (token) =>
-//         token.address === "9NGDi2tZtNmCCp8SVLKNuGjuWAVwNF3Vap5tT8km5er9"
-//     );
-//     const USDC = DEVNET_COIN.find(
-//       (token) => token.address === "zVzi5VAf4qMEwzv7NXECVx5v2pQ7xnqVVjCXZwS9XzA"
-//     );
+    const USDT = DEVNET_COIN.find(
+      (token) =>
+        token.address === "9NGDi2tZtNmCCp8SVLKNuGjuWAVwNF3Vap5tT8km5er9"
+    );
+    const USDC = DEVNET_COIN.find(
+      (token) => token.address === "zVzi5VAf4qMEwzv7NXECVx5v2pQ7xnqVVjCXZwS9XzA"
+    );
 
-//     const pool = await AmmImpl.create(
-//       DEVNET.connection,
-//       new PublicKey(DEVNET_POOL),
-//       USDT!,
-//       USDC!,
-//       {
-//         cluster: DEVNET.cluster as Cluster,
-//       }
-//     );
+    const pool = await AmmImpl.create(
+      DEVNET.connection,
+      new PublicKey(DEVNET_POOL),
+      USDT!,
+      USDC!,
+      {
+        cluster: DEVNET.cluster as Cluster,
+      }
+    );
 
-//     const inAmountALamport = new BN(0.1 * 10 ** pool.tokenA.decimals);
-//     const inAmountBLamport = new BN(0.1 * 10 ** pool.tokenB.decimals);
+    const inAmountALamport = new BN(0.1 * 10 ** pool.tokenA.decimals);
+    const inAmountBLamport = new BN(0.1 * 10 ** pool.tokenB.decimals);
 
-//     const { minPoolTokenAmountOut, tokenAInAmount, tokenBInAmount } =
-//       pool.getDepositQuote(inAmountALamport, inAmountBLamport, false, 1);
+    const { minPoolTokenAmountOut, tokenAInAmount, tokenBInAmount } =
+      pool.getDepositQuote(inAmountALamport, inAmountBLamport, false, 1);
 
-//     const depositTx = await pool.deposit(
-//       mockWallet.publicKey,
-//       tokenAInAmount,
-//       tokenBInAmount,
-//       minPoolTokenAmountOut
-//     );
+    const depositTx = await pool.deposit(
+      mockWallet.publicKey,
+      tokenAInAmount,
+      tokenBInAmount,
+      minPoolTokenAmountOut
+    );
 
-//     try {
-//       const depositResult = await provider.sendAndConfirm(depositTx);
-//       expect(typeof depositResult).toBe("string");
-//       lpBalance = await pool.getUserBalance(mockWallet.publicKey);
-//       expect(lpBalance.toNumber()).toBeGreaterThan(0);
-//     } catch (error: any) {
-//       console.trace(error);
-//       throw new Error(error.message);
-//     }
+    try {
+      const depositResult = await provider.sendAndConfirm(depositTx);
+      expect(typeof depositResult).toBe("string");
+      lpBalance = await pool.getUserBalance(mockWallet.publicKey);
+      expect(lpBalance.toNumber()).toBeGreaterThan(0);
+    } catch (error: any) {
+      console.trace(error);
+      throw new Error(error.message);
+    }
 
-//     const farmingPool = await PoolFarmImpl.getFarmAddressesByPoolAddress(
-//       DEVNET_POOL,
-//       "devnet"
-//     );
-//     farm = await PoolFarmImpl.create(
-//       DEVNET.connection,
-//       farmingPool[0].farmAddress
-//     );
-//   });
+    const farmingPool = await PoolFarmImpl.getFarmAddressesByPoolAddress(
+      DEVNET_POOL,
+      "devnet"
+    );
+    farm = await PoolFarmImpl.create(
+      DEVNET.connection,
+      farmingPool[0].farmAddress
+    );
+  });
 
-//   test("Stake farm", async () => {
-//     const stakeTx = await farm.deposit(mockWallet.publicKey, lpBalance);
-//     try {
-//       const stakeResult = await provider.sendAndConfirm(stakeTx);
-//       expect(typeof stakeResult).toBe("string");
+  test("Stake farm", async () => {
+    const stakeTx = await farm.deposit(mockWallet.publicKey, lpBalance);
+    try {
+      const stakeResult = await provider.sendAndConfirm(stakeTx);
+      expect(typeof stakeResult).toBe("string");
 
-//       stakedBalance = await farm.getUserBalance(mockWallet.publicKey);
+      stakedBalance = await farm.getUserBalance(mockWallet.publicKey);
 
-//       expect(stakedBalance.toNumber()).toBeGreaterThan(0);
-//     } catch (error: any) {
-//       console.trace(error);
-//       throw new Error(error.message);
-//     }
-//   });
+      expect(stakedBalance.toNumber()).toBeGreaterThan(0);
+    } catch (error: any) {
+      console.trace(error);
+      throw new Error(error.message);
+    }
+  });
 
-//   test("Unstake farm", async () => {
-//     const unStakeTx = await farm.withdraw(mockWallet.publicKey, stakedBalance);
-//     try {
-//       const unStakeResult = await provider.sendAndConfirm(unStakeTx);
-//       expect(typeof unStakeResult).toBe("string");
+  test("Unstake farm", async () => {
+    const unStakeTx = await farm.withdraw(mockWallet.publicKey, stakedBalance);
+    try {
+      const unStakeResult = await provider.sendAndConfirm(unStakeTx);
+      expect(typeof unStakeResult).toBe("string");
 
-//       const stakedBalanceMap = await PoolFarmImpl.getUserBalances(
-//         DEVNET.connection,
-//         mockWallet.publicKey,
-//         [farm.address]
-//       );
+      const stakedBalanceMap = await PoolFarmImpl.getUserBalances(
+        DEVNET.connection,
+        mockWallet.publicKey,
+        [farm.address]
+      );
 
-//       expect(stakedBalanceMap.size).toBe(0);
-//     } catch (error: any) {
-//       console.trace(error);
-//       throw new Error(error.message);
-//     }
-//   });
-// });
+      expect(stakedBalanceMap.size).toBe(0);
+    } catch (error: any) {
+      console.trace(error);
+      throw new Error(error.message);
+    }
+  });
+});
 
 describe("Interact with mainnet farm", () => {
   const provider = new AnchorProvider(MAINNET.connection, mockWallet, {
